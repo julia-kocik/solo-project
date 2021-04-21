fcec552400dac464f37b3bbf441dceccff344549
Julia <julia.kocik02@gmail.com>
2021-04-21 17:26:14 +0200

    Add modal, doesn't work

--- src/js/script.js
+++ src/js/script.js
@@ -46,9 +46,57 @@
     });
   };
 
+  //modals
+  //close
+  const closeModal = () => {
+    document.getElementById('overlay').classList.remove('show');
+  };
+  
+  const activateCloseModal = () => {
+    document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
+      btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        closeModal();
+      });
+    });
+
+    document.querySelector('#overlay').addEventListener('click', function(e) {
+      if(e.target === this) {
+        closeModal();
+      }
+    });
+
+    document.addEventListener('keyup', function(e) {
+      if(e.keyCode === 27) {
+        closeModal();
+      }
+    });
+  };
+
+  const openModal = (modal) => {
+    document.querySelectorAll('#overlay > *').forEach(function(modal) {
+      modal.classList.remove('show');
+    });
+    document.querySelectorAll(modal).forEach(function(modal) {
+      modal.classList.remove('show');
+    });
+    document.querySelector('#overlay').classList.add('show');
+    document.querySelector(modal).classList.add('show');
+  };
+
+  const activateOpenModal = () => {
+    document.querySelector('.manager .name').addEventListener('click', (e) => {
+      e.preventDefault();
+      openModal('#myModal');
+    });
+  };
+  
+
   const app = () => {
     initPages();
     initSidebar();
+    activateCloseModal();
+    activateOpenModal();
   };
 
   app();
